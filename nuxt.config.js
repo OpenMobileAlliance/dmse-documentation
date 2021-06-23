df329dfa64cec1088f17cd9247c89ff607a92877
ladyvargas <lady.vargas.santana@gmail.com>
2021-06-23 09:09:26 -0500

    init1

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,6 +4,25 @@ export default theme({
   docs: {
     primaryColor: '#E24F55'
   },
-  router: { base: '/dmse-documentation/' }
+  router: { base: '/dmse-documentation/' },
+  build: {
+    publicPath: '/_nuxt/',
+    babel:{
+      plugins: [
+        ['@babel/plugin-proposal-private-methods', { loose: true }]
+      ]
+    }
+  },
+  generate: {
+    fallback: true
+  },
+  head: {
+    title: "my first nuxt proj - main page",
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' }
+    ],
+    link: [{ rel: 'icon', type: 'image/png', href: './oma-logo.png' }],
+  }
 
 })
