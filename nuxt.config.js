9c181a227703fbaf4032c9b6f4039da9f2b6a0d5
Dejan Dimic <dejan.dimic@gmail.com>
2021-11-27 23:49:18 +0100

    fix: missing media links

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,42 +1,49 @@
-import theme from '@nuxt/content-theme-docs'
+import theme from "@nuxt/content-theme-docs";
 
 export default theme({
-  modules: [
-    ['nuxt-youtube-subscribe-module', {
-      tag: 'YoutubeSubscribeButton'
-    }]
+  components: [
+    { path: "./components/global" },
+    {
+      path: "~~/node_modules/@nuxt/content-theme-docs/src/components/",
+      level: 1,
+    },
   ],
-  target: 'static',
+  modules: [
+    [
+      "nuxt-youtube-subscribe-module",
+      {
+        tag: "YoutubeSubscribeButton",
+      },
+    ],
+  ],
+  target: "static",
   docs: {
-    primaryColor: '#E24F55'
+    primaryColor: "#E24F55",
   },
 
-  router: { 
-    base: '/dmse-documentation/' 
+  router: {
+    base: "/dmse-documentation/",
   },
   build: {
-    publicPath: '/_nuxt/',
-    babel:{
-      plugins: [
-        ['@babel/plugin-proposal-private-methods', { loose: true }]
-      ]
-    }
+    publicPath: "/_nuxt/",
+    babel: {
+      plugins: [["@babel/plugin-proposal-private-methods", { loose: true }]],
+    },
   },
-  
+
   linkedin: {
-    partnerId: '3701497',
-    disabled: false
+    partnerId: "3701497",
+    disabled: false,
   },
-  'youtube-subscribe': {
-    tag: 'YoutubeSubscribeButton'
+  "youtube-subscribe": {
+    tag: "YoutubeSubscribeButton",
   },
-  
+
   generate: {
-    fallback: true
+    fallback: true,
   },
   head: {
     title: "my first nuxt proj - main page",
-    link: [{ rel: 'icon', type: 'image/png', href: './oma-logo.png' }],
-  }
-
-})
+    link: [{ rel: "icon", type: "image/png", href: "./oma-logo.png" }],
+  },
+});
