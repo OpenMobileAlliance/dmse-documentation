75b948b01f567283285c7b2c03bcbff9b638a0fd
jpradocueva <jpradocueva@users.noreply.github.com>
2023-10-03 20:56:19 +0000

    deploy: 59dbef09c1ca8f8108ca3a7b8f5006963e77008c

---
+++ _nuxt/static/1696366575/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Freleases","\u002Fobject-support","\u002Futility-position","\u002Ffaq","\u002Fcontactus","\u002Fenablers","\u002Futilities-overview","\u002Ftools","\u002Fworkshop","\u002Futility-lwm2m-smart-meters","\u002Ftestfests","\u002Flisting"]})
