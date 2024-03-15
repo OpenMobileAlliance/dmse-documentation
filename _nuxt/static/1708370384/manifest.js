3d767d8dae92191b751454467cff298cc3dee3c1
gocadimic <gocadimic@users.noreply.github.com>
2024-03-15 08:17:15 +0000

    deploy: 2392de3c5c20e48d55fd57b8052ff5bdef7a2b9c

--- _nuxt/static/1708370384/manifest.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Freleases","\u002Flisting","\u002Futility-lwm2m-smart-meters","\u002Ftools","\u002Fenablers","\u002Fworkshop","\u002Fcontactus","\u002Ffaq","\u002Fobject-support","\u002Futility-position","\u002Futilities-overview","\u002Ftestfests"]})
